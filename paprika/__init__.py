92b6d9d8c02c3e723296b11c2f4a6576a82c2263
Onno.Meijers <onno.meijers@oxyma.nl>
2018-11-15 13:35:47 +0100

    fix author en harc.json for releases

--- paprika/__init__.py
+++ paprika/__init__.py
@@ -5,5 +5,8 @@ __description__ = 'Paprika a framework for developing (near) real-time marketing
 __uri__ = 'https://gitlab.oxyma.nl/oxyma/paprika.git paprika-master'
 __doc__ = __description__ + ' <' + __uri__ + '>'
 
+__author__ = 'Jan Ripke'
+__email__ = 'onno.meijers@oxyma.nl'
+
 __license__ = 'MIT License'
 __copyright__ = 'Copyright (c) 2018 Oxyma B.V.'
